82d99dc13963b01a2109995de2832c32ea938cbd
Christopherfer <keitaro@chris-no-MacBook-Air.local>
2015-01-13 01:29:22 +0900

    ewe

--- WebContent/B14/labNavi/js/register.js
+++ WebContent/B14/labNavi/js/register.js
@@ -1,17 +1,3 @@
 jQuery(document).ready(function(){
-    var pass = $(':password[name="password"]').val();
-    var repass = $(':password[name="password"]').val();
-    var mail = $(':text[name="mail"]').val();
-    var remail = $(':text[name="remail"]').val();
-jQuery("#userId input:text").on('click blur keydown keyup keypress change',function(){
-     $("#aid").text('半角英数字で入力してください。');
-    var userid = $(':text[name="userId"]').val();
-    if(userid.match(/[^0-9A-Za-z]+/)){
-	$("#aid").text('半角英数字で入力してください。');
-    }else{
-	$("#aid").text('おｋ');
-    }
-});
-
-
+    jQuery("#register").validationEngine();
 });
