f23e671b86ca5870cef7dc8d4f3c995f3093a09f
Christopherfer <keitaro@chris-no-MacBook-Air.local>
2015-01-12 23:43:21 +0900

    aaa

--- WebContent/B14/labNavi/js/register.js
+++ WebContent/B14/labNavi/js/register.js
@@ -1,8 +1,9 @@
+$("#aid").text('半角英数で入力してください。');
 var pass = $(':password[name="password"]').val();
 var repass = $(':password[name="password"]').val();
 var mail = $(':text[name="mail"]').val();
 var remail = $(':text[name="remail"]').val();
-
+	
 jQuery("#userId input:text").on('click blur keydown keyup keypress change',function(){
     var userid = $(':text[name="userId"]').val();
     if(userid.match(/[^0-9A-Za-z]+/)){
