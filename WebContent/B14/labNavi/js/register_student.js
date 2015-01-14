d8f91de919db0c7da83c8dce508bd167bae2cf69
tenma <1210370051g@kindai.ac.jp>
2015-01-14 15:54:05 +0900

    register_student

--- WebContent/B14/labNavi/js/register_student.js
+++ WebContent/B14/labNavi/js/register_student.js
@@ -1,41 +1,18 @@
 function addList(){
-
-<div class="dropdown">
-	<!-- ここが表示されるボタン <a>タグでもOK -->
-		<button class="btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown">
-			Dropdown
-			<span class="caret"></span>
-		</button>
-		<!-- ボタンここまで -->
-
-		<!-- ここはボタンを押すと表示されるリスト -->
-		<ul class="dropdown-menu" role="menu" aria-labelledby="dropdownMenu1">
-			<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Action</a></li>
-			<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Another action</a></li>
-			<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Something else here</a></li>
-			<li role="presentation" class="divider"></li>
-			<li role="presentation"><a role="menuitem" tabindex="-1" href="#">Separated link</a></li>
-		</ul>
-		<!-- リストここまで -->
-	</div>
-
-
-
-
 	var labElement = document.getElementById("labList");
 	var isAssigned = document.getElementById("assigned").checked;
 	var i;
 	if(isAssigned){
 		var labList = ["自然言語処理研究室","交通情報学研究室","知能情報基礎研究室"];
-		var selectElement = document.createElement("select");
+	
+		var str = "<label class='col-sm-2 control-label'></label>"
+			+ "<div class='col-sm-10'>"
+		  + "<select class='form-control'>";
 		for( i=0;i<labList.length;i++){
-			selectElement.setAttribute("name","研究室"); 
-			var inputElement = document.createElement("option");
-			inputElement.setAttribute("value",String(i));
-			inputElement.innerHTML = labList[i];
-			selectElement.appendChild(inputElement);
+			str += "<option value="+String(i)+">"+labList[i]+"</option>";
 		}
-		labElement.appendChild(selectElement);
+		 str += "</select>" + "</div>";
+	   labElement.innerHTML = str;
 	}
 	else{
 		var child = labElement.childNodes;
